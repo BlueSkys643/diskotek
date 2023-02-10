1c177959fedbc22477f841b6dbfc80b565819333
chanmrotzek <nerdtzek14@gmail.com>
2023-02-10 13:25:15 -0800

    cleaned up/polished the command line demo.

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,107 +1,89 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 class User {
-    public String user_first_name;
-    public String user_last_name;
-    public String user_age;
-    public String user_email_address;
-    public String user_username;
-    public String user_password;
+    String firstName, lastName, age, email, username, password;
 
-
-    public User(String user_first_name,String user_last_name, String user_age, String user_email_address,String user_username, String user_password) {
-        this.user_first_name = user_first_name;
-        this.user_last_name = user_last_name;
-        this.user_age = user_age;
-        this.user_email_address = user_email_address;
-        this.user_username = user_username;
-        this.user_password = user_password;
-
+    public User(String firstName, String lastName, String age, String email, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
     @Override
     public String toString() {
-        return  "\n" +
-                "FIRST NAME: " + user_first_name +
-                "\n" +
-                "LAST NAME: " + user_last_name +
-                "\n" +
-                "AGE: " + user_age +
-                "\n" +
-                "EMAIL: " + user_email_address +
-                "\n" +
-                "USERNAME: " + user_username +
-                "\n" +
-                "PASSWORD: " + user_password +
-                "\n" +
-                "User("+user_first_name+", "+user_last_name+", "+user_age+", "+user_email_address+", "+user_username+", "+user_password+")";
-
-
+        return "\nfirstName: " + firstName +
+                "\nlastName: " + lastName +
+                "\nage: " + age +
+                "\nemail: " + email +
+                "\nusername: " + username +
+                "\npassword: " + password +
+                "\n\nUser(" + firstName + ", " + lastName + ", " + age + ", " + email + ", " + username + ", " + password + ")";
     }
 }
 
 public class Main {
-    public static int signup_error_count;
-    public static String user_password;
+    static int errorCount;
 
-    public static String verify_matching_passwords() {
-        System.out.print("Enter a unique password: ");
+    static String verifyPassword() {
         Scanner scanner = new Scanner(System.in);
-        String user_password = scanner.nextLine();
+        System.out.print("Enter a unique password: ");
+        String password = scanner.nextLine();
 
         System.out.print("Confirm your password: ");
-        String user_password_confirmed = scanner.nextLine();
+        String confirmedPassword = scanner.nextLine();
 
-        if (user_password_confirmed.equals(user_password)) {
-            System.out.println("\nYou did it. You actually did it. Bloody wonderful.");
-            return user_password;
+        if (confirmedPassword.equals(password)) {
+            return password;
         } else {
-            if (signup_error_count == 0) {
-                System.out.println("\nHey you gotta confirm the password, okay? Try again.");
-            } else if (signup_error_count == 1) {
-                System.out.println("Bruh it's literally so damn easy, just retype the password.");
-            } else {
-                System.out.println("WHAT DID I JUST SAY, JUST RETYPE THE FUCKING PASSWORD, IT'S SO EASY");
-            }
-
-            signup_error_count++;
-            return verify_matching_passwords();
+            String errorMessage;
+            errorMessage = "\nHm, no match. Please reconfirm your password.";
+            errorCount++;
+            System.out.println(errorMessage);
+            return verifyPassword();
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean password_correct = false;
 
         System.out.print("Enter your first name. ");
-        String user_first_name = scanner.nextLine();
+        String firstName = scanner.nextLine();
 
         System.out.print("Enter your last name. ");
-        String user_last_name = scanner.nextLine();
+        String lastName = scanner.nextLine();
 
         System.out.print("Enter your age. ");
-        String user_age = scanner.nextLine();
+        int age = 0;
 
-        if (Integer.parseInt(user_age) < 21) {
-            int x = 21-Integer.parseInt(user_age);
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                age = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("That's not a numerical value. Please verify your age.");
+            }
+        }
 
-
-            System.out.println("Hey there bucko, you gotta be 21 or older around these parts. Come back in "+x+" years, alright?");
+        if (age < 21) {
+            int yearsLeft = 21 - age;
+            System.out.println("Users must be 21 or older to continue, please return in " + yearsLeft + " years.");
             return;
         }
 
         System.out.print("Enter your email address. ");
-        String user_email_address = scanner.nextLine();
+        String email = scanner.nextLine();
 
-        System.out.print("Enter a username: ");
-        String user_username = scanner.nextLine();
+        System.out.print("Enter a unique username. ");
+        String username = scanner.nextLine();
 
-        while (!password_correct) {
-            user_password = verify_matching_passwords();
-            password_correct = true;
+        String password = verifyPassword();
+
+        User user = new User(firstName, lastName, String.valueOf(age), email, username, password);
+        System.out.println("\nAccount created successfully! Here's the returned user object.");
+        System.out.println(user.toString());
         }
-
-        User user = new User(user_first_name, user_last_name, user_age, user_email_address, user_username, user_password);
-        System.out.println(user);
-    }
-}
+        }
