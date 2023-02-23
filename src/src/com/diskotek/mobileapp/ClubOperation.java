ddd4f8653398b79b3878895e8fe8100a20318869
Greg.Acosta728 <greg.acosta728@gmail.com>
2023-02-23 12:27:41 -0800

    Login GUI

---
+++ src/src/com/diskotek/mobileapp/ClubOperation.java
@@ -0,0 +1,16 @@
+package com.diskotek.mobileapp;
+
+public enum ClubOperation {
+    //Enum for list of operations(named constants) that the various roles have access to.
+    accessEstablishment,
+    openTab,
+    serveDrinks,
+    rejectEntry,
+    enforceRules,
+    manageRoles,
+    manageOperations,
+    manageEmployees,
+    reviewDataDashboard,
+    accessStage,
+    accessGreenRoom
+}
---
+++ src/src/com/diskotek/mobileapp/Role.java
@@ -0,0 +1,6 @@
+package com.diskotek.mobileapp;
+
+public interface Role{ //Role interface
+    String getRoleName();//signature method to get name of the role: getter
+    ClubOperation[] getOperations();//signature method to get the list of operations that role can do: getter
+}
